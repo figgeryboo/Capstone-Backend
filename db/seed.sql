18371219e014bbdfc4d321d2af8e9f03ea24438c
anickacodes <anickalewis@pursuit.org>
2024-06-23 22:42:09 -0400

    fix: debugged- inserted into db feedback table

--- db/seed.sql
+++ db/seed.sql
@@ -1082,5 +1082,5 @@ VALUES
     );
 
 
-INSERT INTO feedback (email, firebase_uid, rating, favoriteFeature, leastFavoriteFeature, suggestions)
+INSERT INTO feedback (email, firebase_uid, rating, favorite_feature, least_favorite_feature, suggestions)
 VALUES ('charlesbarkey@nba.com', 'U7JuriayZDSPe9IHaLikGMQzxyd2', 3, 'Unique eye catching UI', 'Not enough vendors in my area', 'More vendors around Port Authority would be great! I love what you are doing with this app. Keep up the good work!' );
